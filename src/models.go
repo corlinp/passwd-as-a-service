2f8880bcd84f7c42b59ddb08dd6dfcc10ba31882
Corlin Palmer <corlinp@gmail.com>
2018-10-13 21:06:43 -0700

    define User and Group models

---
+++ src/models.go
@@ -0,0 +1,18 @@
+package main
+
+// User represents a UNIX user in a passwd file
+type User struct {
+	Name    string `json:"name"`
+	UID     int    `json:"uid"`
+	GID     int    `json:"gid"`
+	Comment string `json:"comment"`
+	Home    string `json:"home"`
+	Shell   string `json:"shell"`
+}
+
+// Group represents a UNIX group in a groups file
+type Group struct {
+	Name    string `json:"name"`
+	GID     int    `json:"gid"`
+	Members string `json:"members"`
+}
