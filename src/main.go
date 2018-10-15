adb53012f6e0ef9eb40f731090d46a10f2ea97c6
Corlin Palmer <corlinp@gmail.com>
2018-10-14 22:30:48 -0700

    read passwd file on startup

--- src/main.go
+++ src/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"log"
+
 	"github.com/labstack/echo"
 )
 
+var passwdFilePath, groupsFilePath string
+
 func main() {
+	err := readPasswdFile()
+	if err != nil {
+		log.Fatal("Error reading passwd file: ", err.Error())
+	}
+	go watchFiles()
+
 	e := echo.New()
 	e.GET("/healthcheck", healthCheck)
 	e.GET("/users", getUsers)
 	e.GET("/users/:id", getUserByUID)
 	e.GET("/users/query", queryUsers)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func init() {
+	passwdFilePath = "/etc/passwd"
+	groupsFilePath = "/etc/groups"
 }
