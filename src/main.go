cc7ff9ac5f720270f5c6f02d92c981c9ed6849a8
Corlin Palmer <corlinp@gmail.com>
2018-10-14 20:09:38 -0700

    labstack server hello world

--- src/main.go
+++ src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-	fmt.Println("hello")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
