5fb8279092464f7b7f80702466200d608805024e
Corlin Palmer <corlinp@gmail.com>
2018-10-16 23:08:04 -0700

    improve user tests

--- src/users_test.go
+++ src/users_test.go
@@ -76,7 +76,7 @@ func TestUserDB(t *testing.T) {
 	}
 }
 
-func TestReadFile(t *testing.T) {
+func TestReadPasswdFile(t *testing.T) {
 	passwdFilePath = "../test_files/passwd.bad.txt"
 	err := readPasswdFile()
 	if err == nil {
@@ -99,7 +99,7 @@ func TestReadFile(t *testing.T) {
 func TestUserEndpoints(t *testing.T) {
 	passwdFilePath = "../test_files/passwd.txt"
 	assert.NoError(t, readPasswdFile())
-	code, body := mockRequest("/users/78", getUserByUID, "78")
+	code, body := mockParamRequest("/users/78", "/users/:uid", "uid", "78", getUserByUID)
 	assert.Equal(t, http.StatusOK, code)
 	testUser1JSON, _ := json.Marshal(testUser1)
 	assert.Equal(t, testUser1JSON, body)
@@ -120,6 +120,13 @@ func TestUserEndpoints(t *testing.T) {
 	assert.Len(t, users, 1)
 	assert.Equal(t, testUser1, users[0])
 
+	code, body = mockRequest("/users/query?name=bob&uid=0", queryUsers)
+	assert.Len(t, parseUsers(body), 0)
+	code, body = mockRequest("/users/query?uid=78&uid=123", queryUsers)
+	assert.Equal(t, http.StatusBadRequest, code)
+	code, body = mockRequest("/users/query?uid=letter", queryUsers)
+	assert.Equal(t, http.StatusBadRequest, code)
+
 	code, body = mockRequest("/users/search?q=bob", searchUsers)
 	assert.Equal(t, http.StatusOK, code)
 	users = parseUsers(body)
@@ -127,16 +134,28 @@ func TestUserEndpoints(t *testing.T) {
 	assert.Equal(t, testUser1, users[0])
 }
 
-func mockRequest(endpoint string, handler func(c echo.Context) error, uids ...string) (code int, body []byte) {
+func mockRequest(endpoint string, handler func(c echo.Context) error) (code int, body []byte) {
 	e := echo.New()
 	req := httptest.NewRequest(echo.GET, endpoint, nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
-	if len(uids) > 0 {
-		c.SetPath("/users/:uid")
-		c.SetParamNames("uid")
-		c.SetParamValues(uids[0])
+	err := handler(c)
+	if err != nil {
+		fmt.Println(err)
 	}
+	code = rec.Code
+	body = rec.Body.Bytes()
+	return
+}
+
+func mockParamRequest(endpoint, path, paramname, paramvalue string, handler func(c echo.Context) error) (code int, body []byte) {
+	e := echo.New()
+	req := httptest.NewRequest(echo.GET, endpoint, nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath(path)
+	c.SetParamNames(paramname)
+	c.SetParamValues(paramvalue)
 	err := handler(c)
 	if err != nil {
 		fmt.Println(err)
